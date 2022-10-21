2dabdd63ef13bc31135dec0b46f6f973c79abbdc
Qz <qzcurious@gmail.com>
2022-10-22 00:17:41 +0800

    with axios index page size 95.3 kB

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
 import type { NextPage } from "next";
+import axios from "axios";
+
+axios.get("/api/hello").then((res) => {
+  console.log(res.data);
+});
 
 const Home: NextPage = () => {
   return <div>hello world</div>;
